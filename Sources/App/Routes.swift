e8e5f98c54570b0e9b8ff6b7ce1a2409d62f61b2
ShengHua Wu <fantasy0404@gmail.com>
2017-06-28 18:43:19 +0100

    Add postgresql provider.

--- Sources/App/Routes.swift
+++ Sources/App/Routes.swift
@@ -1,4 +1,5 @@
 import Vapor
+import PostgreSQLProvider
 
 extension Droplet {
     func setupRoutes() throws {
@@ -7,19 +8,15 @@ extension Droplet {
             try json.set("hello", "world")
             return json
         }
-
-        get("plaintext") { req in
-            return "Hello, world!"
-        }
-
-        // response to requests to /info domain
-        // with a description of the request
-        get("info") { req in
-            return req.description
-        }
-
-        get("description") { req in return req.description }
         
-        try resource("posts", PostController.self)
+        get("version") { [weak self] req in
+            if let strongSelf = self {
+                let db = try strongSelf.postgresql()
+                let version = try db.raw("SELECT version()")
+                return JSON(node: version)
+            } else {
+                return "No DB connection"
+            }
+        }
     }
 }
