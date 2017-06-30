a826a957b9aaaeffcf01a966e713acb94c7a17b4
ShengHua Wu <fantasy0404@gmail.com>
2017-06-30 19:14:02 +0100

    Group lessons routes.

--- Sources/App/Routes.swift
+++ Sources/App/Routes.swift
@@ -3,53 +3,51 @@ import PostgreSQLProvider
 
 extension Droplet {
     func setupRoutes() throws {
-        get("version") { [weak self] (req) in
-            if let strongSelf = self {
-                let db = try strongSelf.postgresql()
-                let version = try db.raw("SELECT version()")
-                return JSON(node: version)
-            } else {
-                return "No DB connection"
-            }
-        }
-        
-        get("lessons") { (req) in
-            return try JSON(node: Lesson.all().map { try $0.makeJSON() })
-        }
-        
-        post("lessons") { (req) in
-            guard let title = req.data["title"]?.string else {
-                throw Abort.badRequest
+        setupLessonRoutes()
+    }
+}
+
+extension Droplet {
+    func setupLessonRoutes() {
+        group("lessons") { (lessons) in
+            lessons.get("") { (req) in
+                return try JSON(node: Lesson.all().map { try $0.makeJSON() })
             }
             
-            let lesson = Lesson(title: title)
-            try lesson.save()
-            return try lesson.makeJSON()
-        }
-        
-        put("lessons", Int.parameter) { (req) in
-            let id = try req.parameters.next(Int.self)
-            guard let lesson = try Lesson.makeQuery().filter("id", id).first() else {
-                throw Abort.notFound
+            lessons.post("") { (req) in
+                guard let title = req.data["title"]?.string else {
+                    throw Abort.badRequest
+                }
+                
+                let lesson = Lesson(title: title)
+                try lesson.save()
+                return try lesson.makeJSON()
             }
             
-            guard let title = req.data["title"]?.string else {
-                throw Abort.badRequest
+            lessons.put(Int.parameter) { (req) in
+                let id = try req.parameters.next(Int.self)
+                guard let lesson = try Lesson.makeQuery().filter("id", id).first() else {
+                    throw Abort.notFound
+                }
+                
+                guard let title = req.data["title"]?.string else {
+                    throw Abort.badRequest
+                }
+                
+                lesson.title = title
+                try lesson.save()
+                return try lesson.makeJSON()
             }
             
-            lesson.title = title
-            try lesson.save()
-            return try lesson.makeJSON()
-        }
-        
-        delete("lessons", Int.parameter) { (req) in
-            let id = try req.parameters.next(Int.self)
-            guard let lesson = try Lesson.makeQuery().filter("id", id).first() else {
-                throw Abort.notFound
+            lessons.delete(Int.parameter) { (req) in
+                let id = try req.parameters.next(Int.self)
+                guard let lesson = try Lesson.makeQuery().filter("id", id).first() else {
+                    throw Abort.notFound
+                }
+                
+                try lesson.delete()
+                return try lesson.makeJSON()
             }
-            
-            try lesson.delete()
-            return try lesson.makeJSON()
         }
     }
 }
